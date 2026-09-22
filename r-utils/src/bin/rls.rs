54c9f0a76efc26fe615a57d1f473821f4ac1cda9
Tekton <dylanuijthof07@gmail.com>
2026-09-22 11:04:26 +0200

    FEAT: added rkill

--- r-utils/src/bin/rls.rs
+++ r-utils/src/bin/rls.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect(); //wtf does this do
+    let args: Vec<String> = env::args().skip(1).collect();
     let path = if args.is_empty() {"."} else {&args[0]};
 
     let entries = match fs::read_dir(path) {
